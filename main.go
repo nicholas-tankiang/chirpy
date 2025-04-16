be29537d26a0413e65f5d46ecdb9f9b373ea60d2
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2025-04-16 14:09:09 +0800

    add request metrics handlers

--- main.go
+++ main.go
@@ -1,27 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	fileServer := http.FileServer(http.Dir("."))
+	//apiConfig instance for filserverHits counter
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fileServer)))
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.metricsResetHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
-	mux.HandleFunc("/healthz", healthzHandler)
 
 	log.Fatal(server.ListenAndServe())
-
 }
 
-func healthzHandler(w http.ResponseWriter, req *http.Request) {
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+	responseText := fmt.Sprintf("Hits: %d", count)
+	w.Write([]byte(responseText))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("fileserverHits reset to 0"))
 }
